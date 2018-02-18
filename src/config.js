b467f0136c37dcc157553ee194eb9cb27a0f95e3
Jayadev6191 <jaidev.akkiraju@gmail.com>
2018-02-18 11:27:26 -0800

    Added src/config file to gitignore

--- src/config.js
+++ src/config.js
@@ -1,17 +1,14 @@
-// Initialize Firebase
-// import firebase from 'firebase/app';
-// import 'firebase/database';
 var firebase = require('firebase/app');
 require('firebase/database');
 
-// replace below config values with your actual firebase account
+// Uncomment and replace below config values with your actual firebase account
 const DB_CONFIG = {
-    apiKey: "FIREBASE_API_KEY",
-    authDomain: "FIREBASE_AUTH_DOMAIN",
-    databaseURL: "FIREBASE_DATABASE_URL",
-    projectId: "FIREBASE_PROJECT_ID",
-    storageBucket: "FIREBASE_STORAGE_BUCKET",
-    messagingSenderId: "FIREBASE_MESSAGING_SENDER_ID",
+    // apiKey: "FIREBASE_API_KEY",
+    // authDomain: "FIREBASE_AUTH_DOMAIN",
+    // databaseURL: "FIREBASE_DATABASE_URL",
+    // projectId: "FIREBASE_PROJECT_ID",
+    // storageBucket: "FIREBASE_STORAGE_BUCKET",
+    // messagingSenderId: "FIREBASE_MESSAGING_SENDER_ID",
 };
 
 var app = firebase.initializeApp(DB_CONFIG);
